3482e24babb14c0ad4a4649e9604b1e9f28b81fd
Aaron Meese <ajmeese7@gmail.com>
2020-09-23 21:09:33 -0500

    feed2js replaced
    I borrowed some of the code from my aaronmeese.com repo and repurposed it to generate the articles in a style identical to feed2js.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 $.fn.immediateText = function() {
-    // https://stackoverflow.com/a/32170000/6456163
-    return this.contents().not(this.children()).text();
+  // https://stackoverflow.com/a/32170000/6456163
+  return this.contents().not(this.children()).text();
 };
 
 jQuery.fn.textNodes = function() {
-    // https://stackoverflow.com/a/4106957/6456163
-    return this.contents().filter(function() {
-      return (this.nodeType === Node.TEXT_NODE && this.nodeValue.trim() !== "");
-    });
+  // https://stackoverflow.com/a/4106957/6456163
+  return this.contents().filter(function() {
+    return (this.nodeType === Node.TEXT_NODE && this.nodeValue.trim() !== "");
+  });
 }
 
 $(function() {
@@ -19,37 +19,63 @@ $(function() {
     ██║░░██║██║░░██║██║░░██║╚█████╔╝██║░╚███║
     ╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚══╝`);
     console.log("Checking out the internals of my site? Come work with me!");
+    displayMediumArticles();
 
-    $('.scroll-down').click (function() {
-        $('html, body').animate({scrollTop: $('#aboutme').offset().top }, 750);
-        return false;
+    $(".scroll-down").click (function() {
+      $("html, body").animate({scrollTop: $("#aboutme").offset().top }, 750);
+      return false;
     });
-
-    // Set the article header to the RSS title
-    let articlesHeader = document.getElementById("feedTitle");
-    $(".rss-title span").remove();
-    $(".rss-title a").text("Stories on Medium");
-    articlesHeader.innerHTML = $(".rss-title").html();
-    $(".rss-box .rss-title").css("display", "none");
-
-    // Remove all the elements that are comments and not articles
-    let articleMessage = "Continue reading on ";
-    $("li.rss-item").each(function(index, element) {
-        let text = $(this).immediateText();
-        if (!text.includes(articleMessage)) {
-            // Remove any comments, leaving only articles
-            element.remove();
-        } else {
-            text = text.substring(0, text.lastIndexOf(articleMessage));
-            $(this).textNodes().first().replaceWith(text);
-        }
-    });
-
-    // Copy data to my div and delete the script populated HTML
-    let mediumFeed = document.getElementById("mediumFeed");
-    let rssBox = document.getElementsByClassName("rss-box")[0].outerHTML;
-    mediumFeed.innerHTML += rssBox;
-
-    // https://stackoverflow.com/a/15481533/6456163
-    $(".rss-box").eq(1).remove();
 });
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+async function displayMediumArticles() {
+  const articles = await getMediumArticles();
+  const rssItems = document.getElementsByClassName("rss-items")[0];
+  while (articles.length == 0) await sleep(250);
+
+  for (let i = 0; i < 3; i++) {
+    let articleData = articles[i];
+    let article = document.createElement("li");
+    article.className = "rss-item";
+
+    let title = document.createElement("a");
+    title.className = "rss-title";
+    title.innerText = articleData.title;
+    title.href = articleData.url;
+    article.appendChild(title);
+    article.appendChild(document.createElement("br"));
+
+    let date = document.createElement("span");
+    date.className = "rss-date";
+    date.innerText = `${new Date(articleData.createdAt)}`.slice(0, 28);
+    article.appendChild(date);
+    article.appendChild(document.createElement("br"));
+
+    let desc = document.createElement("p");
+    desc.innerText = articleData.description;
+    article.appendChild(desc);
+
+    rssItems.appendChild(article);
+  }
+}
+
+async function getMediumArticles() {
+  let articles = [];
+
+  const getMediumData = (nextArticle) => {
+    if (!nextArticle) nextArticle = "";
+    const url = `https://medium-feed.ajmeese7.workers.dev?username=ajmeese7&next=${nextArticle}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(async json => {
+        if (json.data.posts.length === 0) return;
+        json.data.posts.forEach(post => articles.push(post));
+        await getMediumData(json.next);
+      })
+      .catch(error => console.error(error.message))
+      .finally(() => articles = articles.sort((a, b) => (a.createdAt > b.createdAt) ? -1 : 1));
+  };
+
+  await getMediumData();
+  return articles;
+}
